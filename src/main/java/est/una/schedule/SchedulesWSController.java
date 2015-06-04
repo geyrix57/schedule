52244bd4ed472abaf07b7c46dd6a2923fe17bea4
josanuz <josanuz@hotmail.es>
2015-06-03 22:40:23 -0600

    ServiceWeb

--- src/main/java/est/una/schedule/SchedulesWSController.java
+++ src/main/java/est/una/schedule/SchedulesWSController.java
@@ -1,7 +1,12 @@
 package est.una.schedule;
 
+import est.una.schedule.model.Curso;
 import est.una.schedule.model.CursoListWrapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Casa on 03/06/2015.
@@ -10,8 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SchedulesWSController {
     @RequestMapping(value = "/schedule", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-    public String schedule(@RequestBody CursoListWrapper cursoList) {
-        for (String s : cursoList.getCursos()) System.out.println(s);
-        return "OKAS";
+    public Curso schedule(@RequestBody CursoListWrapper cursoList) {
+
+        String[] cursos = new String[cursoList.getCursos().size()];
+        for (int i = 0; i < cursoList.getCursos().size(); i++)
+            cursos[i] = cursoList.getCursos().get(i);
+        ScheduleGenerator scheduleGenerator = new ScheduleGenerator(cursos);
+
+        Curso c = new Curso();
+        c.setCodigo("EIF203");
+        return c;
+
     }
 }
--- src/main/java/est/una/schedule/model/CursoListWrapper.java
+++ src/main/java/est/una/schedule/model/CursoListWrapper.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class CursoListWrapper {
     private List<String> cursos;
 
+    public CursoListWrapper() {
+    }
+
     public CursoListWrapper(boolean b) {
         cursos = new ArrayList<>();
     }
